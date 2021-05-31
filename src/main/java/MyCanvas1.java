b2f9d054725dbd283136c34c0006bf9b3db99c0e
jozefxcabala <xcabala@stuba.sk>
2021-05-31 13:01:07 +0200

    Creating canvas

---
+++ src/main/java/MyCanvas1.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+
+public class MyCanvas1 extends Canvas {
+    public MyCanvas1(){
+        super();
+        this.setSize(500,500);
+        this.setBackground(Color.WHITE);
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+    }
+}
--- src/main/java/MyWindow1.java
+++ src/main/java/MyWindow1.java
@@ -1,7 +1,21 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class MyWindow1 extends JFrame {
+    private MyCanvas1 canvas;
+    private BorderLayout layout;
+
     public MyWindow1(String nameOfWindow){
         super(nameOfWindow);
+
+        this.layout = new BorderLayout();
+        this.setLayout(this.layout);
+
+        this.canvas = new MyCanvas1();
+        this.add(this.canvas);
+
+        this.pack();
+        this.setVisible(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
