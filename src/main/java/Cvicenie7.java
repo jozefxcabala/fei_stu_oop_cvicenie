5f55001bad685ebea1bd90286cb2e46260a80f32
jozefxcabala <xcabala@stuba.sk>
2021-05-31 12:57:18 +0200

    Initial commit

---
+++ src/main/java/Cvicenie7.java
@@ -0,0 +1,5 @@
+public class Cvicenie7 {
+    public static void main(String[] args) {
+        MyWindow1 myWindow1 = new MyWindow1("Uloha 7.1");
+    }
+}
---
+++ src/main/java/MyWindow1.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+
+public class MyWindow1 extends JFrame {
+    public MyWindow1(String nameOfWindow){
+        super(nameOfWindow);
+    }
+}
