29dd430353f6756f53d5f47be5b825851a0863b2
jozefxcabala <xcabala@stuba.sk>
2021-05-31 13:34:23 +0200

    Rotating line

--- src/main/java/MyCanvas1.java
+++ src/main/java/MyCanvas1.java
@@ -9,6 +9,22 @@ public class MyCanvas1 extends Canvas {
 
     @Override
     public void paint(Graphics g) {
-        super.paint(g);
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setStroke(new BasicStroke(5));
+        for(int i = 0; i <= 360; i++){
+            double j = i * Math.PI / 360;
+            int x1 = (int)(140-100*Math.sin(j));
+            int y1 = (int)(130+100*Math.cos(j));
+            int x2 = (int)(140+100*Math.sin(j));
+            int y2 = (int)(130-100*Math.cos(j));
+            g.setColor(Color.RED);
+            g.drawLine(x1,y1,x2,y2);
+            try {
+                Thread.sleep(15);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            g.clearRect(0, 0, this.getWidth(), this.getHeight());
+        }
     }
 }
